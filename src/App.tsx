99bd67abfe85497cb8bf86b5fad0d4687157d134
anclaev <iahugo@yandex.ru>
2021-05-25 21:09:18 +0400

    Роутинг.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
-const App = () => <div className="App">React App</div>
+import { Switch, Route } from 'react-router'
+
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Home from './pages/Home'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/log" component={Login} />
+    <Route exact path="/reg" component={Register} />
+  </Switch>
+)
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-import './index.sass'
+import './styles/index.sass'
+
 import App from './App'
 
 render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
   document.body
 )
